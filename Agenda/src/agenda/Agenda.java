9756e6ed3928304c503486282c3bfa64817a5655
Erick Osorio <evgom.sid@gmail.com>
2017-05-25 14:11:34 -0500

    La clase Agenda ya inicia la interfaz gráfica.

--- Agenda/src/agenda/Agenda.java
+++ Agenda/src/agenda/Agenda.java
@@ -5,9 +5,7 @@
  */
 package agenda;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import ventanas.Interfaz;
 
 /**
  *
@@ -19,26 +17,6 @@ public class Agenda {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Persona persona1 = new Persona("Bello", "Mena", "Luis Raúl", 'H',
-                new GregorianCalendar(1992, 3 - 1, 13), Entidad.COLIMA);
-        System.out.println(persona1.getCURP());
-        persona1.setEmail("evgom@gmsd.com");
-        System.out.println(persona1.getEmail());
-
-        Persona persona2 = new Persona("Osorio", "Merlos", "Erick Victor Gabriel",
-                'h', new GregorianCalendar(1987, Calendar.SEPTEMBER, 03), Entidad.DISTRITO_FEDERAL);
-        System.out.println(persona2.getCURP());
-
-        GregorianCalendar calendario = new GregorianCalendar(1987, Calendar.SEPTEMBER, 03);
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy MM dd");
-        System.out.println(sdf.format(calendario.getTime()));
-
-        persona1.setTel("55");
-        persona1.setTel(" 55");
-        persona1.setTel(" 55 33");
-        persona1.setTel("AA");
-        persona1.setTel("55*");
-
+        Interfaz.main(args);
     }
-
 }
